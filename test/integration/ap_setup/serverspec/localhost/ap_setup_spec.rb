c94fe62c8583520a9abc579c511a117581cf482f
maruoka.takashi <maruoka.takashi@tis.co.jp>
2015-11-09 10:41:37 +0900

    Delete the spec because carried out in the deploy stage

--- test/integration/ap_setup/serverspec/localhost/ap_setup_spec.rb
+++ test/integration/ap_setup/serverspec/localhost/ap_setup_spec.rb
@@ -34,12 +34,6 @@ describe 'ap_default' do
     expect(file('/usr/share/tomcat7/lib/postgresql-9.4-1201.jdbc41.jar')).to be_file
   end
 
-  it 'is created context xml' do
-    expect(file('/etc/tomcat7/Catalina/localhost/jpetstore.xml'))
-      .to be_file
-      .and be_owned_by('tomcat')
-  end
-
   it 'is tomcat service enabled and running' do
     expect(service('tomcat7')).to be_enabled.and be_running
   end
