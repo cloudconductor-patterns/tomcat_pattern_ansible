28709e3b8c78c0dee553cdbb7a86cb7069748fed
Yo Takezawa <takezawa.yo@tis.co.jp>
2016-03-01 10:17:17 +0900

    Install ruby from source

--- prepare.sh
+++ prepare.sh
@@ -13,42 +13,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+RUBY_VERSION=2.3.0
+RUBY_URL="https://cache.ruby-lang.org/pub/ruby/${RUBY_VERSION%\.*}/ruby-${RUBY_VERSION}.tar.gz"
+
 run() {
   output="$("$@" 2>&1)"
   status="$?"
 }
 
-install_git() {
-  run which git
-  if [ $status -ne 0 ]; then
-    echo "install git."
-    yum install -y git
-    if [ $status -ne 0 ] ; then
-      echo "$output" >&2
-      return 1
-    fi
-  fi
-}
-
 install_ruby() {
   run which ruby
   if [ $status -ne 0 ]; then
-    echo "install ruby."
-
-    run which git
-    if [ $status -ne 0 ]; then
-      install_git
-    fi
-    yum install -y openssl-devel readline-devel zlib-devel tar
-    git clone https://github.com/sstephenson/rbenv.git /usr/local/rbenv
-    git clone https://github.com/sstephenson/ruby-build.git /usr/local/rbenv/plugins/ruby-build
-    echo 'export RBENV_ROOT="/usr/local/rbenv"' > /etc/profile.d/rbenv.sh
-    echo 'export PATH="${RBENV_ROOT}/bin:${PATH}"' >> /etc/profile.d/rbenv.sh
-    echo 'eval "$(rbenv init --no-rehash -)"' >> /etc/profile.d/rbenv.sh
-    source /etc/profile.d/rbenv.sh
-    rbenv install -v 2.2.3
-    rbenv rehash
-    rbenv global 2.2.3
+    echo "install ruby"
+    yum install -y make gcc gcc-c++ autoconf openssl-devel
+    curl --retry 5 -s ${RUBY_URL} > /tmp/ruby-${RUBY_VERSION}.tar.gz
+    tar zxvf /tmp/ruby-${RUBY_VERSION}.tar.gz -C /tmp
+    cd /tmp/ruby-${RUBY_VERSION}
+    ./configure
+    make
+    make install
   fi
 }
 
@@ -67,14 +50,12 @@ install_ansible() {
 set_ruby_path() {
   run which ruby
   if [ $status -ne 0 ]; then
-    if [[ -d /opt/chefdk ]] && [[ -x /opt/chefdk/embedded/bin/ruby ]]; then
-      ruby_home=/opt/chefdk/embedded
-    elif [[ -d /opt/chef ]] && [[ -x /opt/chef/embedded/bin/ruby ]]; then
-      ruby_home=/opt/chef/embedded
+    if [[ -x /usr/local/bin/ruby ]]; then
+      ruby_path=/usr/local/bin
     fi
 
-    echo "export PATH=\$PATH:${ruby_home}/bin" > ${CHEF_ENV_FILE}
-    export PATH=${ruby_home}/bin:${PATH}
+    echo "export PATH=\$PATH:${ruby_path}" > ${CHEF_ENV_FILE}
+    export PATH=${PATH}:${ruby_path}
   fi
 }
 
@@ -127,6 +108,7 @@ setup_python_env() {
   fi
 }
 
+install_ruby
 install_ansible
 install_serverspec
 
